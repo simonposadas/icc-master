4bf4601d47b67385d273e1ea1cb87ddbecb6e583
gjhuerte <gjhuerte@gmail.com>
2017-10-21 01:29:56 +0800

    Updated UI For Approval/Disapproval Modal
    - added computation for budget for food based on total budget - ( budget for equipments + budget for workers )

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,6 +4,36 @@
 
 @section('content')
 
+<style>
+
+	.line-either-side {
+		overflow: hidden;
+		text-align: center;
+	}
+
+	.line-either-side:before,
+	.line-either-side:after {
+		background-color: #e5e5e5;
+		content: "";
+		display: inline-block;
+		height: 1px;
+		position: relative;
+		vertical-align: middle;
+		width: 50%;
+	}
+
+	.line-either-side:before {
+		right: 0.5em;
+		margin-left: -50%;
+	}
+
+	.line-either-side:after {
+		left: 0.5em;
+		margin-right: -50%;
+	}
+
+</style>
+
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
@@ -64,71 +94,87 @@
 <div class="modal fade" tabindex="-1" role="dialog" id="editModal">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Approve Reservation?</h4>
-            </div>
-            {!! Form::open() !!}
-            {{ method_field('PUT') }}
-            <input type="hidden" class="id" name="id">
+            <div class="modal-body">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <legend><h3 class="text-muted">Reservation Approval</h3></legend>
+              {!! Form::open() !!}
+              {{ method_field('PUT') }}
+              <input type="hidden" class="id" name="id">
 
-            <!-- client info -->
-            <label class='control-label'><u>Client</u></label> <br>
-            <div id='client-info'></div>
+              <div class="panel panel-primary" style="border:none;border-radius:0px;">
+                <!-- client info -->
+                <div class='panel-heading'>Client Details</div>
+                <div class="panel-body">
+                  <div id='client-info' style="list-group" class="text-muted"></div>
+                </div>
+              </div>
 
-            <!-- event detail -->
-            <label class='control-label'><u>Event Detail</u></label> <br>
-            <div id='event-detail'></div>
+              <div class="panel panel-info" style="border:none;border-radius:0px;">
+                <!-- client info -->
+                <div class='panel-heading'>Event Details</div>
+                <div class="panel-body">
+                  <div id='event-detail' class="text-muted"></div>
+                </div>
+              </div>
 
-            <!-- package detail -->
-            <label class='control-label'><u>Package Detail</u></label> <br>
-            <div id='package-detail'></div>
+              <div class="panel panel-success" style="border:none;border-radius:0px;">
+                <!-- client info -->
+                <div class='panel-heading'>Package Detail</div>
+                <div class="panel-body">
+                  <div id='package-detail' class="text-muted"></div>
+                </div>
+              </div>
 
-            <!-- disapprove reason -->
-            <div id='div-disapprove-reason' class='form-group'>
-                <br>
-                <label class='control-label'><u>Disapprove Reason</u></label> 
-                {!! Form::textarea('disapprove_reason', '', ['class' => 'form-control']) !!}    
-            </div>
+              <!-- reservation budget -->
+              <div id='div-bud_food' class='form-group'>
+                  <label>Budget for Food</label>
+                  <br />
+                  <span class="text-muted" style="font-size:12px">The following will be updated based on the inputted data on Equipment and Workers Budget</span>
+                  <input type='number' readonly name='budget_food' class='form-control' id="budget_food" style="background-color:white;border-radius:0px;" placeholder="Amount in Php allocated for Food" required/>
+              </div>
 
-            <!-- reservation budget -->
-            <div id='div-bud_food' class='form-group'>
-                <br>
-                <label class='control-label'><u>Budget for Food</u></label> 
-                <input type='number' name='budget_food' class='form-control' required/>
-            </div>
+                <h5 class="text-muted line-either-side">Anong meron dito?</h5>
+                <div class="row to-add">
+                  <div class="col-sm-6">
+                   <div class='form-group'>
+                     <input name = "description" class="form-control" style="border-radius:0px;" placeholder="Description"></input>
+                   </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class='form-group'>
+                      <input name = "price" class="form-control add-each" style="border-radius:0px;" placeholder="Price"></input>
+                    </div>
+                  </div>
+                </div>
 
+                <button id = "button-add" class="btn btn-success btn-sm" type = "button">ADD</button>
 
-<div class="row to-add">
-  <div class="col-sm-6">
-   <div class='form-group'><input name = "description" class="form-control" placeholder="Description"></input></div>
-</div>
-  <div class="col-sm-6">
-<div class='form-group'><input name = "price" class="form-control add-each" placeholder="Price"></input></div>
-</div>
-</div>
-               
-<button id = "button-add" type = "button">ADD</button>
+              <div id='div-bud_equip' class='form-group'>
+                  <br>
+                  <label class='control-label'>Budget for Equipment</label>
+                  <input type='number' name='budget_equip' class='form-control' style="border-radius:0px;" id="budget_equip" placeholder="Amount in Php allocated for Equipments" required/>
+              </div>
 
+              <div id='div-bud_worker' class='form-group'>
+                  <br>
+                  <label class='control-label'>Budget for Workers</label>
+                  <input  type='number' name='budget_work' class='form-control' style="border-radius:0px;" id="budget_work" placeholder="Amount in Php allocated for Workers" required/>
+              </div>
 
-            <div id='div-bud_equip' class='form-group'>
-                <br>
-                <label class='control-label'><u>Budget for Equipment</u></label> 
-                <input type='number' name='budget_equip' class='form-control' required/>
-            </div>
-
-            <div id='div-bud_worker' class='form-group'>
-                <br>
-                <label class='control-label'><u>Budget for Workers</u></label> 
-                <input  type='number' name='budget_work' class='form-control' required/>
+              <!-- disapprove reason -->
+              <div id='div-disapprove-reason' class='form-group'>
+                  <br>
+                  <label class='control-label'>Reason for Disapproval</label>
+                  {!! Form::textarea('disapprove_reason', '', ['class' => 'form-control' , 'rows' => '4' , 'style' => 'border-radius:0px;']) !!}
+              </div>
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn" id='btn-submit'>Approve</button>
+                <button type="submit" class="btn" id='btn-submit' style="border-radius:0px; padding:8px;">Approve</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal" style="border-radius:0px; padding:8px;">Close</button>
             </div>
-            {!! Form::close() !!}
-        </div>
+              {!! Form::close() !!}
+            </div>
     </div>
 </div>
 @endsection
@@ -158,7 +204,7 @@
                         );
 
                 $('#editModal #event-detail').html(
-                        'Budget: &#8369;' + response.cust_budget + "<br>" +
+                        'Budget: &#8369; <span id="customer_budget_amount">' + response.cust_budget + "</span><br>" +
                         'Type: ' + event_detail.event_type + '<br>' +
                         'Place: ' + event_detail.place + '<br><br>'
                         );
@@ -180,11 +226,48 @@
         $('#editModal').modal('show');
     });
 
+    $('#budget_work').on('focus keyup keydown keypress',function(){
+      console.log('typing workers budget ...')
+      computeBudgetForFood()
+    })
+
+    $('#budget_equip').on('focus keyup keydown keypress',function(){
+      console.log('typing equipment budget ...')
+      computeBudgetForFood()
+    })
+
+    function computeBudgetForFood()
+    {
+        remaining_value = 0;
+        customer_budget = $('#customer_budget_amount').text();
+        budget_for_workers = $('#budget_work').val();
+        budget_for_equipments = $('#budget_equip').val();
+
+        remaining_value = customer_budget
+
+        if(customer_budget >= 0)
+        {
+          if(budget_for_workers > 0)
+          {
+            remaining_value = remaining_value - budget_for_workers
+          }
+
+          if(budget_for_equipments > 0)
+          {
+              remaining_value = remaining_value - budget_for_equipments
+          }
+
+        }
+
+        $('#budget_food').text(remaining_value)
+        $('#budget_food').val(remaining_value)
+    }
+
     $('#editModal #btn-submit').on('click', function () {
         $(this).attr('disabled', 'disabled');
         $('#editModal form').submit();
     });
-    
+
     $('#editModal').on('hide.bs.modal', function () {
         $('#editModal #btn-submit').removeClass('btn-primary');
         $('#editModal #btn-submit').removeClass('btn-danger');
@@ -197,7 +280,7 @@ $(document).on('click', '#button-add', function(){
           '<div class="col-sm-6">' +
         '<div class="form-group"><input class="form-control add-each" name = "price[]" placeholder="Price"></input></div>' +
         '</div>');
-         
+
 });
 
 
@@ -213,7 +296,7 @@ $(document).on('change', '.add-each', function()
     });
 
     $('input [name = "budget_food"]').val(budget);
-   
+
 });
 
 
